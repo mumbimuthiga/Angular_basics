6a22bababda8d62ee970c8b2d3f50d0d9b8272f9
Mumbi Muthiga <veronicmuthiga.com>
2022-06-29 13:57:06 +0300

    Add files

---
+++ src/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+//Decorators
+@Component ({
+    selector:'app-server',
+    templateUrl:'./server.component.html'
+})
+
+export class ServerComponent{
+
+}
