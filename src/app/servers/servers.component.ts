aadf02d0785193c5848f9b845ca24f7e115ff9ab
Mumbi Muthiga <veronicmuthiga.com>
2022-06-30 06:24:45 +0300

    Add files

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,16 +3,41 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   //selector: '[app-servers]',
   selector:'.app-servers',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>`,
+ // template: `
+  //<app-server></app-server>
+  //<app-server></app-server>`,
+
+  templateUrl:'./servers.component.html',
+
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  //Property Binding
+  allowNewServer=false;
+  constructor(){
+    setTimeout(()=>{
+      this.allowNewServer=true;
+    },2000);
   }
+  //End of data binding
+  ngOnInit(): void {
+    
+  }
+  //DataBinding(Communication between typescript and html)
+  //String Interpolation
+  serverId:number=10;
+  serverStatus:String='offline';
+
+  getServerStatus(){
+    return this.serverStatus;
+  }
+
+  
+  
+ 
+
+  
+
+  
 
 }
